91366dc8ce0d5a7b5e6f3b4e6e3c4b66241f019e
Aleksey Petrukhin <alexnord95@gmail.com>
2021-01-01 14:43:46 +0300

    AlexGif ver.1.0 (empty spring-boot app)

---
+++ src/main/java/ru/petrukhin/alexgif/AlexgifApplication.java
@@ -0,0 +1,13 @@
+package ru.petrukhin.alexgif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexgifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlexgifApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/petrukhin/alexgif/AlexgifApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petrukhin.alexgif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexgifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
